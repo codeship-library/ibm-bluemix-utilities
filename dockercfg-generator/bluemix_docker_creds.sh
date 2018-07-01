48a5c7fd46d1da6736f90afa1e195ada9138663c
Joe Siewert <joe.siewert@codeship.com>
2018-06-30 22:21:45 -0500

    Testing

--- dockercfg-generator/bluemix_docker_creds.sh
+++ dockercfg-generator/bluemix_docker_creds.sh
@@ -5,13 +5,15 @@ set -e
 : "${BLUEMIX_API_KEY:?Need to set your BLUEMIX_API_KEY}"
 : "${BLUEMIX_API_ENDPOINT:=api.ng.bluemix.net}"
 
-# Logging into IBM Bluemix
+# Logging into IBM Cloud
 echo "Logging into IBM Cloud"
-bluemix login --apikey "${BLUEMIX_API_KEY}" -a "${BLUEMIX_API_ENDPOINT}"
-bluemix cs init
-bluemix cr login
+bx login --apikey "${BLUEMIX_API_KEY}" -a "${BLUEMIX_API_ENDPOINT}"
+#bluemix cs init
+bx cr login
+bx cr namespace-list
 
 # writing aws docker creds to desired path
 echo "Writing Docker creds to $1"
 chmod 544 ~/.docker/config.json
 cp ~/.docker/config.json $1
+cat ~/.docker/config.json
